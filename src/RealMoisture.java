206380a04f5d887cca5413f8a5452845a32ad73e
yjjunnie <161452557+yjjunnie@users.noreply.github.com>
2025-04-01 02:42:48 +0800

    added code into sensor classes + simulator + realconditions

---
+++ src/RealMoisture.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+
+class RealMoisture implements Simulator {
+    int day_diff = climenu.getCurrentDay() - dayLastCleared; //use for randomiser ??
+    private MoistureSensor moistureSensor;
+    private Random random = new Random();
+
+    // Bidirectional association
+    public void setMoistureSensor(MoistureSensor moistureSensor) {
+        this.moistureSensor = moistureSensor;
+    }
+    public MoistureSensor getMoistureSensor() {
+        return moistureSensor;
+    }
+
+    // Constructor 
+    public RealMoisture(MoistureSensor moistureSensor) {
+
+    }
+
+    @Override
+    public int getCondition() {
+        return moistureSensor.getCondition();
+    }
+
+    @Override
+    public void setCondition(String condition) {
+        double probability = Simulator.probability(moistureSensor.getConditionType());
+        moistureSensor.condition = randomizer(probability);
+    }
+
+    @Override
+    public int randomizer(double prob) {
+        int min = 0, max = 100;
+        int randomizedValue = getRandomInt(min, max) * day_diff;
+        return (int) Math.round(randomizedValue * prob);
+    }
+}
