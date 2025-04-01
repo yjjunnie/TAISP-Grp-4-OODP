e12a31251354e55c535c42c1a656ba59212a465e
Atharshlakshmi Vijayakumar <77614793+atharshlakshmi@users.noreply.github.com>
2025-04-01 16:02:27 +0800

    Update AquaticCrop.java

--- src/AquaticCrop.java
+++ src/AquaticCrop.java
@@ -1,6 +1,33 @@
-class AquaticCrop extends Crop {
-    public AquaticCrop(String name, int seedlingTime, int matureTime) {
+import java.util.HashMap;
+
+public class AquaticCrop extends Crop{
+
+    public AquaticCrop(String name, int seedlingTime, int matureTime, int[] temperature, int[] humidity, int[] lightExposure) {
         super(name, seedlingTime, matureTime);
+
+        // get the hash map
+        HashMap<String, int[]> conditions = this.getConditions();
+
+        // set conditions
+        String[] aquaticConditions = new String[]{"Temperature", "Humidity", "Light Exposure"};
+        this.setConditionName(aquaticConditions);
+
+        // fill hashmap
+        for (String condition : this.getConditionName()) {
+            switch (condition) {
+                case "Temperature":
+                    conditions.put(condition, temperature);
+                    break;
+                case "Humidity":
+                    conditions.put(condition, humidity);
+                    break;
+                case "Light Exposure":
+                    conditions.put(condition, lightExposure);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-    // Additional aquatic-specific behavior can be added here
+
 }
