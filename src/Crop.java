e08371706e39ba9a44734f4f39b79eed452d9e95
Atharshlakshmi Vijayakumar <77614793+atharshlakshmi@users.noreply.github.com>
2025-04-01 16:03:00 +0800

    Update Crop.java

--- src/Crop.java
+++ src/Crop.java
@@ -1,13 +1,68 @@
-class Crop {
-    public String name;
+import java.util.HashMap;
+
+public class Crop {
+    private String name;
     public int seedlingTime;
     public int matureTime;
-    public List<String> conditionList;
+    private HashMap <String, int[]> conditions;
+    private String[] conditionName;
 
     public Crop(String name, int seedlingTime, int matureTime) {
         this.name = name;
         this.seedlingTime = seedlingTime;
         this.matureTime = matureTime;
-        this.conditionList = new ArrayList<>();
+        this.conditions = new HashMap<>();
+        this.conditionName = new String[]{};
+    }
+
+    // prints conditions neatly, can be changed later on
+    public void printConditions() {
+        for (String condition: conditions.keySet()) {
+            System.out.println(condition + ": ");
+            System.out.print("Min: " + conditions.get(condition)[0]);
+            System.out.println(", Max: " + conditions.get(condition)[1]);
+        }
+    }
+
+    public int getSeedlingTime() {
+        return seedlingTime;
+    }
+
+    public void setSeedlingTime(int seedlingTime) {
+        this.seedlingTime = seedlingTime;
+    }
+
+    public int getMatureTime() {
+        return matureTime;
+    }
+
+    public void setMatureTime(int matureTime) {
+        this.matureTime = matureTime;
+    }
+
+    // below are get set for variables
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HashMap<String, int[]> getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(HashMap<String, int[]> conditions) {
+        this.conditions = conditions;
+    }
+
+    public String[] getConditionName() {
+        return conditionName;
+    }
+
+    public void setConditionName(String[] conditionName) {
+        this.conditionName = conditionName;
     }
 }
+
