1b405c7e4841edbd33d67b86b8e294ca90524d2f
Atharshlakshmi Vijayakumar <77614793+atharshlakshmi@users.noreply.github.com>
2025-04-01 16:02:00 +0800

    Update LandCrop.java

--- src/LandCrop.java
+++ src/LandCrop.java
@@ -1,6 +1,35 @@
-class LandCrop extends Crop {
-    public LandCrop(String name, int seedlingTime, int matureTime) {
+import java.util.HashMap;
+
+public class LandCrop extends Crop {
+
+    public LandCrop(String name, int seedlingTime, int matureTime,  int[] temperature, int[] humidity, int[] lightExposure, int[] soilMoisture) {
         super(name, seedlingTime, matureTime);
+
+        // get the hash map
+        HashMap<String, int[]> conditions = this.getConditions();
+
+        // set conditions
+        String[] landConditions = new String[]{"Temperature", "Humidity", "Light Exposure", "Soil Moisture"};
+        this.setConditionName(landConditions);
+
+        // fill hashmap
+        for (String condition : this.getConditionName()) {
+            switch (condition) {
+                case "Temperature":
+                    conditions.put(condition, temperature);
+                    break;
+                case "Humidity":
+                    conditions.put(condition, humidity);
+                    break;
+                case "Light Exposure":
+                    conditions.put(condition, lightExposure);
+                    break;
+                case "Soil Moisture":
+                    conditions.put(condition, soilMoisture);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-    // Additional land-specific behavior can be added here
 }
