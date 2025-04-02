d8264243ba290947a9f5e2a75c8d8a0167044376
azhicles <ytf.timothy@gmail.com>
2025-04-02 15:16:41 +0800

    edited enum string representations

--- src/ConditionType.java
+++ src/ConditionType.java
@@ -1,6 +1,17 @@
 public enum ConditionType {
-    HUMIDITY,
-    TEMPERATURE,
-    MOISTURE,
-    LIGHT
+    HUMIDITY("humidity"),
+    TEMPERATURE("temperature"),
+    MOISTURE("moisture"),
+    LIGHT("light exposure");
+
+    private String conditionName;
+
+    ConditionType(String conditionName) {
+        this.conditionName = conditionName;
+    }
+
+    @Override
+    public String toString() {
+        return this.conditionName;
+    }
 }
--- src/GrowthStage.java
+++ src/GrowthStage.java
@@ -1,5 +1,16 @@
 public enum GrowthStage {
-    SEED,
-    SEEDLING,
-    MATURE
+    SEED("seed"),
+    SEEDLING("seedling"),
+    MATURE("mature");
+
+    private String growthStageName;
+
+    GrowthStage(String growthStageNameName) {
+        this.growthStageName = growthStageNameName;
+    }
+
+    @Override
+    public String toString() {
+        return this.growthStageName;
+    }
 }
